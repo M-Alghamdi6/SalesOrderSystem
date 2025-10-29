b3ba8289eaa176ad663e38fd2731613b24a28a6c
M-Alghamdi6 <M-Alghamdi6+ghamdi888m@gmail.com>
2025-10-29 10:48:55 +0300

    LoginForm

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { LogInForm } from './Pages/log-in-form/log-in-form';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LogInForm],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
